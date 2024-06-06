c9a8e4b68a47f2b3b44e8f8897f3a6c3b673c90c
hemanth <hmnthskmr@gmail.com>
2024-06-06 19:25:52 +0530

    demogithub committed

---
+++ DEMO GITHUB/src/DEMOGITHUB/demo_github.java
@@ -0,0 +1,16 @@
+package DEMOGITHUB;
+
+public class demo_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		 
+		System.out.println("a");
+				
+		
+
+	}
+
+}
